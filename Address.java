32b9f38df6df69425a8765dc0b87f128324915b0
MahmoudAlSaffarini <6297190@edu.vaniercollege.qc.ca>
2025-12-05 21:59:38 -0500

    Commit Address Class

---
+++ Address.java
@@ -0,0 +1,70 @@
+package org.Mahmoud;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Address {
+
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    /**
+     * Checks if postalCode is valid or not using the CDCDCD format.
+     * @param postalCode the input postal code.
+     * @return true if postal Code uses the CDCDCD format, else false.
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        postalCode = postalCode.toUpperCase();
+
+        for (int i = 0; i < postalCode.length(); i++) {
+            char ch = postalCode.charAt(i);
+
+            if (i % 2 == 0) {
+                if (!Character.isLetter(ch)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(ch)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetNo=" + streetNo +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", province=" + province +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
+    }
+}
---
+++ Province.java
@@ -0,0 +1,14 @@
+package org.Mahmoud;
+
+public enum Province {
+    AB,
+    BC,
+    MB,
+    NB,
+    NL,
+    NS,
+    ON,
+    PE,
+    QC,
+    SK
+}
