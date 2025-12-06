ee7846ef62b7cd0159a66f0602310ae076a93e82
MahmoudAlSaffarini <6297190@edu.vaniercollege.qc.ca>
2025-12-06 16:04:25 -0500

    Add Student and Course Classes

---
+++ Course.java
@@ -0,0 +1,3 @@
+package org.Mahmoud;
+
+public class Course {}
---
+++ Student.java
@@ -0,0 +1,49 @@
+package org.Mahmoud;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Student {
+
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registeredCourses;
+    private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId);
+        nextId++;
+
+        this.studentName = Util.toTitleCase(studentName);
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        }
+        registeredCourses.add(course);
+
+        return true;
+    }
+
+    public boolean
+
+
+}
